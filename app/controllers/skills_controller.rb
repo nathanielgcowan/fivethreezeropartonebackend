3f82f7eb9faa9060f2f22afaa8b9db56ff706dbf
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-05-30 00:37:00 -0500

    this is to add the rest of the skills in the database

---
+++ app/controllers/skills_controller.rb
@@ -0,0 +1,28 @@
+class SkillsController < ApplicationController
+
+    def index
+        skills = Skill.all
+        render json: skills
+    end
+
+    def create
+        skill = Skill.new
+        
+        if skill.save
+            render json: skill
+        else
+            render json: {errors: skill.errors.full_messages}
+        end
+    end
+
+    def destroy
+        skill = Skill.find(params[:id])
+        skill.destroy
+    end
+    
+    private
+
+    def skill_params
+        params.require(:skill).permit(:name, :creature_id)
+    end
+end
